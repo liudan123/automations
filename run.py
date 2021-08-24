66a96b983c41bfead259a984af9400ba6aff632b
liudan123 <18846450275@163.com>
2021-08-24 17:05:50 +0800

    提交代码

--- run.py
+++ run.py
@@ -1,16 +1,29 @@
 # -*- encoding:utf-8 -*-
 
-from automations.testcase.test_http_request import TestHttp
+# from automations.testcase.test_yiduiyi_request import TestHttp
+from automations.testcase.test_manager_request import TestHttp
+from automations.tools.project_path import *
 import unittest
-import HTMLTestRunnerCN
-from tools.project_path import *
 
-suite = unittest.TestSuite()
-loader = unittest.TestLoader()
-suite.addTest(loader.loadTestsFromTestCase(TestHttp))
+from HTMLTestRunner import HTMLTestRunner
+from automations.tools.project_path import *
+
+# suite = unittest.TestSuite()
+# loader = unittest.TestLoader()
+# suite.addTest(loader.loadTestsFromTestCase(TestHttp))
+#
+
+
+
+
+# 1、初始化testloader
+testloader = unittest.TestLoader()
+
+# 2、查找测试用例,加载
+suite = testloader.discover(case_dir)
 
 with open(test_report_path,'wb') as file:
-    runner = HTMLTestRunnerCN.HTMLTestRunner(
+    runner = HTMLTestRunner(
         stream=file,
         verbosity=2,
         title="单元测试报告",
@@ -18,3 +31,4 @@ with open(test_report_path,'wb') as file:
         tester="么么哒"
     )
     runner.run(suite)
+
